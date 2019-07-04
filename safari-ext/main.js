97bf7bff950e66b8cd3bba135fe11fe1d04ac84d
Betim Sherifi <bs40388@ubt-uni.net>
2019-07-04 10:11:09 +0200

    sites added

--- safari-ext/main.js
+++ safari-ext/main.js
@@ -2,8 +2,14 @@ var API_URL_SUBMIT = "http://0.0.0.:3000/api/entry/submit";
 var API_URL_GET_CHECKED_FACTS = "http://0.0.0.0:3000/api/get-page-fact-check-requests";
 
 var sites = [
-    {name: 'Telegrafi', domain: 'telegrafi.com', css_classes: ['.article-title', '.article-area'], elements: ['h1','p'] }
-]
+    { name: 'Blic', domain: 'blic.rs', css_classes: ['.article-body', '.article-item'], css_elements: ['p', 'h1'] },
+    { name: 'Espreso', domain: 'espreso.rs', css_classes: ['.titleWrap', '.articleTxt'], css_elements: ['p', 'h1', 'strong'] },
+    { name: 'Kurir', domain: 'kurir.rs', css_classes: ['.articleTxt', '.titleWrap'], css_elements: ['p', 'h1'] },
+    { name: 'SrbijaDanas', domain: 'srbijadanas.com', css_classes: ['.article__content-wrapper'], css_elements: ['p', 'h1', 'h2', 'div'] },
+    { name: 'Telegraf', domain: 'telegraf.rs', css_classes: ['.article-body'], css_elements: ['p', 'h1', 'h2'] },
+    { name: 'N1Info', domain: 'n1info.com', css_classes: ['.single-article'], css_elements: ['h1', 'p'] },
+    { name: 'Danas', domain: 'danas.rs', css_classes: ['.site-main'], css_elements: ['h1', 'p'] },
+ ]
 
 safari.application.addEventListener("command", function(event) {
     if (event.command == 'contextmenu') {
